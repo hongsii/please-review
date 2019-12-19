e0cd780c897c1cb310af8b5e853004b58535aa16
hongsii <hongsii.dev@gmail.com>
2019-12-20 00:48:39 +0900

    JPA 설정 추가

---
+++ please-review-core/src/main/kotlin/please/review/core/config/JpaConfiguration.kt
@@ -0,0 +1,8 @@
+package please.review.core.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfiguration
